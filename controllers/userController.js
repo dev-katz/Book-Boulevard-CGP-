085d1901d173293e21007188f6e221b9fa87872f
Prabudda Perera <72686198+Katz2003@users.noreply.github.com>
2024-03-19 13:29:23 +0530

    code adjust

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,21 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
+exports.register = async (req, res) => {
+    try {
+        const { firstname, lastname, email, password } = req.body;
+        const userExists = await User.findOne({ email });
+        if (userExists) return res.status(400).json({ message: 'User already exists' });
 
+        const user = new User({firstname, lastname, email, password});
+        await user.save();
+
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
 
 exports.login = async (req, res) => {
     try {
@@ -10,10 +25,11 @@ exports.login = async (req, res) => {
             return res.status(401).json({ message: 'Incorrect username or password' });
         }
 
+        // Generate JWT token
         const token = jwt.sign(
             { userId: user._id, username: user.username },
-            process.env.JWT_SECRET,
-            { expiresIn: '1h' }
+            process.env.JWT_SECRET, // Ensure you have this environment variable set
+            { expiresIn: '1h' } // Token expires in 1 hour
         );
 
         res.status(200).json({ message: 'Login successful', token });
