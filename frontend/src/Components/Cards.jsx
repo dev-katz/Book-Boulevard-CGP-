0b5e639fce0b391f37a54ea4db706e6f3fb85fd6
Katz2003 <72686198+Katz2003@users.noreply.github.com>
2024-03-25 13:47:59 +0530

    books cards

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -1,40 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import './Css/Cards.css';
+import style from './Css/Cards.css';
 import card from '../bookcards.json';
 import ProfileSection from './ProfileSection';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const CardsPage = () => {
-  const [cards, setCards] = useState([]);
+    const [cards, setCards] = useState([]);
 
-  useEffect(() => {
-    fetch('data.json')
-      .then(response => response.json())
-      .then(data => setCards(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+    useEffect(() => {
+        fetch('data.json')
+            .then(response => response.json())
+            .then(data => setCards(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
 
-  return (
-    <div className="cards-page">
-      <ProfileSection username="Your Username" profileImage="profile.jpg" />
-      <div className="gallery">
-        {card.map(card => (
-          <div key={card.id} className="card">
-            <div className="book-image">
-              <img src={card.image} alt={card.title} />
+    return (
+
+        <div className="cards-page">
+            <Navbar />
+            <ProfileSection username="Yevin Mawathage" profileImage="profile.jpg" />
+            <div className="gallery">
+                {card.map(card => (
+                    <div key={card.id} className="card">
+                        <div className="book-image">
+                            <img src={card.image} alt={card.title} className='cardimage'/>
+                        </div>
+                        <div className="card-content">
+                            <div className="card-details">
+                                <h2 className='cardheading'>{card.title}</h2>
+                                <p className='cardpara'><strong></strong> {card.author}</p>
+                                <p className='card-des'>{card.description}</p>
+                                <button className='cardbutton'>View</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div className="card-content">
-              <div className="card-details">
-                <h2>{card.title}</h2>
-                <p><strong></strong> {card.author}</p>
-                <p>{card.description}</p>
-                <button>View</button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+            <Footer />
+        </div>
+
+    );
 };
 
 export default CardsPage;
